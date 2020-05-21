2c777db6006f1dd6771ce1638e9771e6538a0c53
MayronGMZ <mayron.souza@gmz.solutions>
2020-05-21 13:27:38 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const pets = [
+  {
+    name: 'matosão',
+    type: 'dog',
+    age: 10,
+  },
+  {
+    name: 'rex',
+    type: 'dog',
+    age: 5
+  },
+  {
+    name: 'dolly',
+    type: 'cat',
+    age: 2
+  },
+  {
+    name: 'tico',
+    type: 'rabbit',
+    age: 3
+  },
+  {
+    name: 'tereré',
+    type: 'fish',
+    age: '1'
+  }
+]
+
+console.log(pets)
